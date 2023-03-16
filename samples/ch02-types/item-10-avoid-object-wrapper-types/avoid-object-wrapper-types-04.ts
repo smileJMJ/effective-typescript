b4fd13609f7dd041523ee10838d54308676793a4
jmj0324 <jmj0324@ncsoft.com>
2023-03-16 16:02:54 +0900

    2장 공부중 - 2

--- samples/ch02-types/item-10-avoid-object-wrapper-types/avoid-object-wrapper-types-04.ts
+++ samples/ch02-types/item-10-avoid-object-wrapper-types/avoid-object-wrapper-types-04.ts
@@ -1,3 +1,6 @@
 const s: String = "primitive";
 const n: Number = 12;
 const b: Boolean = true;
+
+type A = typeof BigInt(11); // BigIntConstructor
+const A = typeof BigInt(11); // var BigInt: BigIntConstructor(value: string | number | bigint | boolean) => bigint
